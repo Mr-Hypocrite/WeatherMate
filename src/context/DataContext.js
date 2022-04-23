d0b6e7ca0c555eeef056ba03896c01c2ab52bb27
Mr-Hypocrite <ym.jatin@gmail.com>
2022-04-23 20:27:11 +0530

    Add ContextHook

--- src/context/DataContext.js
+++ src/context/DataContext.js
@@ -1,11 +1,30 @@
-import { createContext } from "react"
+import { createContext, useReducer, useState } from "react"
 
 export const DataContext = createContext()
 
+const themeReducer = (url, action) => {
+    switch (action.type) {
+        case 'CHANGE_URL': 
+            return { url: action.payload }
+
+        default:
+            return url
+    }
+}
+
 export function DataProvider({ children }) {
+
+    const [url, dispatch] = useReducer(themeReducer, {
+        url: '',
+    })
+
+    const changeURL = (url) => {
+        dispatch({ type: 'CHANGE_URL', payload: url})
+    }
+
     return(
-        <DataContext.Provider>
-            
+        <DataContext.Provider value={{ url, changeURL }}>
+            { children }
         </DataContext.Provider>
     )
 }
--- src/hooks/useData.js
+++ src/hooks/useData.js
@@ -1,2 +1,13 @@
 import { useContext } from "react"
 import { DataContext } from "../context/DataContext"
+
+export const useTheme = () => {
+
+    const context = useContext(DataContext)
+
+    if (context === undefined) {
+        throw new Error("useTheme() must be use inside a ThemeProvider")
+    }
+
+    return context
+}
